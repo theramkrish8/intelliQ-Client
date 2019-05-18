1eb301e46654f93bf35d5ef6b4fa3e9925463d01
Ramakrishna Srinivasamurty <theramkrish8@gmail.com>
2019-05-18 17:44:27 +0000

    localhost is replaced with IP
    Signed-off-by: Ramakrishna Srinivasamurty <theramkrish8@gmail.com>

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://35.200.183.136:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
